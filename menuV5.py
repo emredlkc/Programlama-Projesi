f022f1956929bac1313fd998f89176cb7de0aa69
BrkGrc <115698781+BrkGrc@users.noreply.github.com>
2023-01-06 19:19:10 +0300

    menuV6
    Menü en boy oranı değiştirildi

--- menuV5.py
+++ menuV5.py
@@ -1,20 +1,21 @@
 import pygame 
 import sys
-import pygame_gui
-from pygame.locals import * 
+import pygame as pg
 from subprocess import call
-import pygame as pg 
-pygame.init()
+from pygame.locals import *
+import pygame_gui
 
-en , boy = 1000 ,  700 
-ekran =pygame.display.set_mode((en, boy ))
-pygame.display.set_caption('text inputlama')
-font =pygame.font.Font("font.ttf" , 20)
 
-MANAGER = pygame_gui.UIManager((en , boy ))
 mainClock= pygame.time.Clock()
-TEXT_INPUT= pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((275,100 ), (500,50)), manager=MANAGER, 
-                                                        object_id='#main_text_entry'  )
+
+pygame.init()
+pygame.display.set_caption('menü')
+en= 564
+boy = 650
+ekran = pygame.display.set_mode((en,boy))
+font =pygame.font.Font("font.ttf" , 20)
+MANAGER = pygame_gui.UIManager((en , boy ))
+#TEXT_INPUT1= pygame_gui.elements.UITextEntryLine(relative_rect=pygame.Rect((275,100 ), (500,50)), manager=MANAGER, object_id='#main_text_entry'  )
 
 
 
@@ -25,37 +26,18 @@ class Background(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.left, self.rect.top = location
 
-
-
-
-
-
-
-
 def show_user_name(user_name):
-
-
-        
-    run = True 
-    ekran_renk = 50,0,50
-
-    
+    run = True
 
     while run:
-
-        ekran.fill((ekran_renk))
-
-        # yazi_yaz('menü ', font , (255,25,25),ekran, 20 , 20)
-
         a , b  = pygame.mouse.get_pos()
-                
+        BackGround = Background('images1_1000x700.png', [0,0])
+        ekran.blit(BackGround.image, BackGround.rect)
 
         surf1 =font.render('ÇIK', True , 'white')
         surf2 = font.render('BAŞLA',True , 'white')
         button_1 = pygame.Rect(400,350,200,100)
         button_2 = pygame.Rect(400,500,200,100)
-
-
 
 
         for event in pygame.event.get():
@@ -75,10 +57,7 @@ def show_user_name(user_name):
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if button_1.collidepoint(event.pos):
-
                     game()    
-
-
 
         if button_1.x <= a <= button_1.x +200 and button_1.y <= b <= button_1.y +100:
             pygame.draw.rect(ekran,(255,0,0),button_1)
@@ -98,13 +77,7 @@ def show_user_name(user_name):
         ekran.blit(new_text,new_text_rect)
         pygame.display.update()
 
-        
-
         mainClock.tick(60)
-
-
-
-
 
 
 def yazi_yaz(yazi , font , renk , surface ,x , y ):
@@ -115,30 +88,20 @@ def yazi_yaz(yazi , font , renk , surface ,x , y ):
 
 
 
-
-
 def main_menu():
     run = True 
-    ekran_renk = 50,0,50
-
     
-
     while run:
-
-        ekran.fill((ekran_renk))
-
-        yazi_yaz('Adınızı giriniz: ', font , (255,25,25),ekran, 275 , 75)
-
         a , b  = pygame.mouse.get_pos()
-                
+        BackGround = Background('arkaplanmenu.jpg', [0,0])
+        ekran.blit(BackGround.image, BackGround.rect)
+        yazi_yaz('Adınızı giriniz: ', font , (255,25,25),ekran, 275 , 75)
 
         surf1 =font.render('ÇIK', True , 'white')
         surf2 = font.render('BAŞLA',True , 'white')
-        button_1 = pygame.Rect(400,350,200,100)
-        button_2 = pygame.Rect(400,500,200,100)
+        button_1 = pygame.Rect(190,300,200,100)
+        button_2 = pygame.Rect(190,450,200,100)
         UI_REFRESH_RATE= mainClock.tick(60)/1000
-
-
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -165,9 +128,6 @@ def main_menu():
 
 
 
-
-
-
         if button_1.x <= a <= button_1.x +200 and button_1.y <= b <= button_1.y +100:
             pygame.draw.rect(ekran,(255,0,0),button_1)
         else:
@@ -181,43 +141,35 @@ def main_menu():
         ekran.blit(surf1, (button_2.x +75 , button_2.y+40))
 
 
-
-
         MANAGER.draw_ui(ekran)
         print(a,b)
         pygame.display.update()
         mainClock.tick(60)
         MANAGER.update(UI_REFRESH_RATE)
 
+        
 
 
 
 def game():
     calis = True
     while calis:
-
         ekran.fill((0,0,0))
         call(["python", "protot.py"])
-        yazi_yaz('game', font ,(255,255,255),ekran , 20,20)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
 
             if event.type == KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                        main_menu()
-
-
-
-
-
+                if event.key == K_ESCAPE:
+                    main_menu()
+                      
 
 
 
         pygame.display.flip()
         pygame.display.update()
         mainClock.tick(60)
-
 
 
 
